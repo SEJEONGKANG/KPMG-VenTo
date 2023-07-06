689d8200e1fe92674a3ba07ab7b4e2031266b898
junwoo <anjunwoo990809@gmail.com>
2023-07-07 06:40:38 +0900

    deleting cachce

--- frontend/src/components/dashboard/tasks-progress.js
+++
@@ -1,6 +0,0 @@
-import { Avatar, Box, Card, CardContent, Grid, LinearProgress, Typography } from '@mui/material';
-import InsertChartIcon from '@mui/icons-material/InsertChartOutlined';
-
-export const TasksProgress = (props) => (
-  <a></a>
-);
--- frontend/src/components/dashboard/total-profit.js
+++
@@ -1,6 +0,0 @@
-import { Avatar, Card, CardContent, Grid, Typography } from '@mui/material';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-
-export const TotalProfit = (props) => (
-  <a></a>
-);
